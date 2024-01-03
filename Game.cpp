0e32dc05d5ffd11dc55b71bbdb74052f80443dd2
BakunovMaxym <max.2006@ukr.net>
2024-01-03 23:38:43 +0200

    added plants

--- Game.cpp
+++ Game.cpp
@@ -3,7 +3,7 @@
 void Game::Innit()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	window = SDL_CreateWindow("Terraria", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, CAMERA_WIDTH, CAMERA_HEIGHT, SDL_WINDOW_FULLSCREEN);
+	window = SDL_CreateWindow("Terraria", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, CAMERA_WIDTH, CAMERA_HEIGHT, 0);
 
 	renderer = SDL_CreateRenderer(window, 0, 0);
 	running = true;
@@ -24,15 +24,16 @@ void Game::Innit()
 
 	int heights[MAP_WIDTH];
 	int heights2[MAP_WIDTH];
-	heights[0] = rand() % 30 + 50;
+	heights[0] = rand() % 30 + 80;
 	heights2[0] = rand() % 15 + 5;
-	for (size_t x = 1; x < MAP_WIDTH; x++)
+	for (size_t x = 1; x < MAP_WIDTH; x++)//generate surface
 	{
-		if (rand() % 3==1) {
+		int num = rand() % 3;
+		if (num ==1) {
 			heights[x] = heights[x - 1] + 1;
 
 		}
-		else if (rand() % 3 == 1) {
+		else if (num == 2) {
 			heights[x] = heights[x - 1];
 		
 		}
@@ -45,13 +46,15 @@ void Game::Innit()
 		}
 
 	}
-	for (size_t x = 1; x < MAP_WIDTH; x++)
+	cameraPos = { 0,heights[(CAMERA_WIDTH / 2) / BLOCK_SIZE] * BLOCK_SIZE - CAMERA_HEIGHT / 2 - 2*BLOCK_SIZE };//set camera to the ground
+	for (size_t x = 1; x < MAP_WIDTH; x++)//height of dirt layer
 	{
-		if (rand() % 3 == 1) {
+		int num = rand() % 3;
+		if (num == 1) {
 			heights2[x] = heights2[x - 1] + 1;
 
 		}
-		else if (rand() % 3 == 1) {
+		else if (num == 2) {
 			heights2[x] = heights2[x - 1];
 
 		}
@@ -64,23 +67,58 @@ void Game::Innit()
 		}
 
 	}
-	for (int x = 0; x < MAP_WIDTH; x++) {
+	for (int x = 0; x < MAP_WIDTH; x++) {//filling map with blocks
+		int id_ground = rand() % 40;
 		for (int y = heights[x]; y < MAP_HEIGHT; y++) {
 			if (y == heights[x]) {
 				Map[y][x] = 3;
+				if (id_ground >= 0 && id_ground <= 3) Map[y - 1][x] = 12;//add plants
+				if (id_ground >= 4 && id_ground <= 7) Map[y - 1][x] = 13;
+				if (id_ground >= 10 && id_ground <= 11) Map[y - 1][x] = 88;
+				if (id_ground >= 12 && id_ground <= 13) Map[y - 1][x] = 89;
+				if (id_ground >= 14 && id_ground <= 15) Map[y - 1][x] = 90;
+				if (id_ground >= 16 && id_ground <= 17) Map[y - 1][x] = 91;
+				if (id_ground >= 18 && id_ground <= 19) Map[y - 1][x] = 92;
+				if (id_ground >= 20 && id_ground <= 21) Map[y - 1][x] = 93;
+				if (id_ground >= 22 && id_ground <= 23) Map[y - 1][x] = 94;
+				if (id_ground >= 24 && id_ground <= 25) Map[y - 1][x] = 95;
+				if (id_ground == 26) Map[y - 1][x] = 15;
+				if (id_ground >= 27 && id_ground <= 31) {
+					int tree_height = rand() % 15 + 7;
+					if (Map[y - 1][x - 1] != 20 && Map[y - 2][x - 1] != 20) {
+						for (int i = y - 1; i >= y - tree_height; i--) {
+							Map[i][x] = 20;
+							if (i == y - tree_height) {
+									for (int j = i; j >= i - 4; j--) {
+								for (int p = x - 2; p <= x + 2; p++) {
+									if ((j == i && p == x - 2) || (j == i && p == x + 2) || (j == i - 4 && p == x - 2) || (j == i - 4 && p == x + 2) || (Map[j][p] == 20)) {
+										continue;
+									}
+											Map[j][p] = 145;
+										}
+									}
+								}
+							}
+						}
 
-			}
-			else if (y < heights[x] + heights2[x]+5)
-			{
-				Map[y][x] = 2;
-			}
-			else
-			{
-				Map[y][x] = 1;
+					}
+				}
+			
+				else if (y < heights[x] + heights2[x] + 5)
+				{
+					Map[y][x] = 2;
+				}
+				else
+				{
+					Map[y][x] = 1;
+
+				}
+			int lake_chance = rand() % 150;
+
 			}
 		}
 	}
-}
+
 
 void Game::Update()
 {
@@ -121,12 +159,12 @@ void Game::Render()
 	Vector2 firstPos = { cameraPos.x / BLOCK_SIZE,cameraPos.y / BLOCK_SIZE };
 
 	int textureIndex = 1;
-	for (size_t i = 0; i < CAMERA_WIDTH/BLOCK_SIZE; i++)
+	for (size_t i = 0; i < CAMERA_WIDTH/BLOCK_SIZE; i++)//filling screen with blocks
 	{
 		for (size_t j = 0; j < CAMERA_HEIGHT / BLOCK_SIZE; j++)
 		{
 			textureIndex = Map[firstPos.y + j][firstPos.x + i];
-			SDL_Rect sours = { textureIndex* TEXTURE_SIZE ,textureIndex/100 * TEXTURE_SIZE ,TEXTURE_SIZE,TEXTURE_SIZE };
+			SDL_Rect sours = { textureIndex%16* TEXTURE_SIZE ,textureIndex/16 * TEXTURE_SIZE ,TEXTURE_SIZE,TEXTURE_SIZE };
 			SDL_Rect dest = { i * BLOCK_SIZE,j * BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE };
 			SDL_RenderCopy(renderer, texture, &sours, &dest);
 
@@ -141,9 +179,9 @@ void Game::Render()
 	textureIndex = 5;
 	SDL_Rect sours = { textureIndex * TEXTURE_SIZE , textureIndex / 100 * TEXTURE_SIZE ,TEXTURE_SIZE,TEXTURE_SIZE };
 	SDL_Rect dest = { mouseX * BLOCK_SIZE, mouseY * BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE };
-	SDL_RenderCopy(renderer, texture, &sours, &dest);
+	SDL_RenderCopy(renderer, texture, &sours, &dest);//render coursor
 
-	dest = {playerPos.x, playerPos.y, 128, 128};
+	dest = {playerPos.x, playerPos.y, 128, 128};//set character position
 	SDL_RenderCopy(renderer, hoe, NULL, &dest);
 	SDL_RenderPresent(renderer);
 }
--- Settings.h
+++ Settings.h
@@ -2,10 +2,10 @@
 const int MAP_WIDTH = 2000;
 const int MAP_HEIGHT = 300;
 
-const int CAMERA_WIDTH = 2560;
-const int CAMERA_HEIGHT = 1440;
+const int CAMERA_WIDTH = 1280;
+const int CAMERA_HEIGHT = 1024;
 
-const int BLOCK_SIZE = 32;
+const int BLOCK_SIZE = 20;
 const int TEXTURE_SIZE = 16;
 struct Buttons {
 	bool w, a, s, d;
