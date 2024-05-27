e34c2bf9ea2e11dfb71c95e9610bc76034ea19e4
Ivankarpe <aakarpenko2013@gmail.com>
2024-05-27 12:26:11 +0300

    gitingnore cahges

--- Settings.h
+++ Settings.h
@@ -14,7 +14,7 @@ const int MAP_WIDTH = 2000;
 const int MAP_HEIGHT = 300;
 const int FULLSCREEN = 0;
 const int WATERCAPACITY = 500;
-const int MAX_LIGHT = 10;
+const int MAX_LIGHT = 11;
 
 const int CAMERA_WIDTH = 1920;
 const int CAMERA_HEIGHT = 1080;
